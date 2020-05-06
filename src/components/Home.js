deb249d556bca2b938a18094b02b139df9eca15e
weilyuwang <weilyuwang@gmail.com>
2020-05-05 22:26:23 -0400

    fetch movies data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+// config
+import {
+    API_URL,
+    API_KEY,
+    IMAGE_BASE_URL,
+    POSTER_SIZE,
+    BACKDROP_SIZE,
+} from "../config";
 
 // import components
 import HeroImage from "./elements/HeroImage";
@@ -8,15 +17,57 @@ import MovieThumb from "./elements/MovieThumb";
 import Spinner from "./elements/Spinner";
 import LoadMoreBtn from "./elements/LoadMoreBtn";
 
-const Home = () => (
-    <>
-        <HeroImage />
-        <SearchBar />
-        <Grid />
-        <MovieThumb />
-        <Spinner />
-        <LoadMoreBtn />
-    </>
-);
+const Home = () => {
+    const [state, setState] = useState({ movies: [] });
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    const fetchMovies = async (endpoint) => {
+        setError(false);
+        setLoading(true);
+
+        try {
+            // fetch() is async, json() is also async function
+            const result = await (await fetch(endpoint)).json();
+            console.log(result);
+            /*
+            result = {
+                page: 1,
+                results: [],
+                total_pages: 500,
+                total_results: 10000
+            }
+            */
+
+            setState((prev) => ({
+                ...prev,
+                movies: [...result.results],
+                heroImage: prev.heroImage || result.results[0], // take the previous state's heroImage if exists, otherwise take the first result image
+                currentPage: result.page,
+                totalPages: result.total_pages,
+            }));
+        } catch (error) {
+            setError(true);
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchMovies(`${API_URL}movie/popular?api_key=${API_KEY}`);
+    }, []);
+
+    return (
+        <>
+            <HeroImage />
+            <SearchBar />
+            <Grid />
+            <MovieThumb />
+            <Spinner />
+            <LoadMoreBtn />
+        </>
+    );
+};
 
 export default Home;
--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 // To se the latest configuration fetch it from https://api.themoviedb.org/3/configuration?api_key=019e8f375549e0bbd4a4191862ebc88f
 // Read more about the API here: https://developers.themoviedb.org/
 
-import API_KEY_SECRET from "./APIKey";
+import { API_KEY_SECRET } from "./APIKey";
 
 const API_URL = "https://api.themoviedb.org/3/";
 const API_KEY = API_KEY_SECRET;
