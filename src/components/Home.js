87014df2775f7af00b4760a9b7f21f5c80656203
weilyuwang <weilyuwang@gmail.com>
2020-05-06 15:20:13 -0400

    refactor to use custom hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,46 +17,13 @@ import MovieThumb from "./elements/MovieThumb";
 import Spinner from "./elements/Spinner";
 import LoadMoreBtn from "./elements/LoadMoreBtn";
 
+// custom hook
+import { useHomeFetch } from "./hooks/useHomeFetch";
+
 const Home = () => {
-    const [state, setState] = useState({ movies: [] });
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+    const [{ state, loading, error }, fetchMovies] = useHomeFetch();
 
-    const fetchMovies = async (endpoint) => {
-        setError(false);
-        setLoading(true);
-
-        try {
-            // fetch() is async, json() is also async function
-            const result = await (await fetch(endpoint)).json();
-            console.log(result);
-            /*
-            result = {
-                page: 1,
-                results: [],
-                total_pages: 500,
-                total_results: 10000
-            }
-            */
-
-            setState((prev) => ({
-                ...prev,
-                movies: [...result.results],
-                heroImage: prev.heroImage || result.results[0], // take the previous state's heroImage if exists, otherwise take the first result image
-                currentPage: result.page,
-                totalPages: result.total_pages,
-            }));
-        } catch (error) {
-            setError(true);
-            console.log(error);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    useEffect(() => {
-        fetchMovies(`${API_URL}movie/popular?api_key=${API_KEY}`);
-    }, []);
+    console.log(state);
 
     return (
         <>
---
+++ src/components/hooks/useHomeFetch.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+// config
+import { API_URL, API_KEY } from "../../config";
+
+export const useHomeFetch = () => {
+    const [state, setState] = useState({ movies: [] });
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    const fetchMovies = async (endpoint) => {
+        setError(false);
+        setLoading(true);
+
+        try {
+            // fetch() is async, json() is also async function
+            const result = await (await fetch(endpoint)).json();
+
+            /*
+            result = {
+                page: 1,
+                results: [],
+                total_pages: 500,
+                total_results: 10000
+            }
+            */
+
+            setState((prev) => ({
+                ...prev,
+                movies: [...result.results],
+                heroImage: prev.heroImage || result.results[0], // take the previous state's heroImage if exists, otherwise take the first result image
+                currentPage: result.page,
+                totalPages: result.total_pages,
+            }));
+        } catch (error) {
+            setError(true);
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchMovies(`${API_URL}movie/popular?api_key=${API_KEY}`);
+    }, []);
+
+    return [{ state, loading, error }, fetchMovies];
+};
